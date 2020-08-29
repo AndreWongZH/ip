f77a94ff5250d7315b32dd9bf0a0c06050851ad1
AndreWongZH <wongandre98@gmail.com>
2020-08-29 21:48:16 +0800

    Add new text automation testing
    Lets create tests for our program using I/O redirection technique.
    
    This is because in the future we can be notified if any of our test
    failed due to new changes being made.

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -7,7 +7,15 @@ REM delete output from previous run
 del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac  -cp ..\src -Xlint:none -d ..\bin ..\src\main\java\Duke.java
+javac  -cp ..\src -Xlint:none -d ..\bin ^
+..\src\main\java\Duke.java ^
+..\src\main\java\TaskManager.java ^
+..\src\main\java\TaskType.java ^
+..\src\main\java\Task.java ^
+..\src\main\java\Todo.java ^
+..\src\main\java\Deadline.java ^
+..\src\main\java\Event.java
+
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
@@ -15,7 +23,7 @@ IF ERRORLEVEL 1 (
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
+java -Dfile.encoding=UTF-8 -classpath ..\bin Duke < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
