94dae75eefc790304022fb5711b0b8527edaf65a
AndreWongZH <wongandre98@gmail.com>
2020-08-29 22:45:28 +0800

    Refactor Duke class for readability
    Duke class: extract methods from Duke main class
    
    Duke class now have varying levels of abstraction within the main
    method.
    
    Lets make the main method in Duke class to have the same level of
    abstraction by extracting out lines of code into a method.
    I also added static string constants for greetings and named
    constants to avoid magic numbers.
    
    Have the same level of abstraction allows us to adhere to SLAP
    principle.
    Not having magic numbers increases readability

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,52 +1,75 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static final String GREET = "____________________________________________________________\n" +
+            " __\n" +
+            "('')  Hello! I'm MK.II Titan with feelings.\n" +
+            "// \\  What can I do for you?\n" +
+            "|_/| \n" +
+            "____________________________________________________________\n";
+
+    private static final String END = "____________________________________________________________\n" +
+            " __\n" +
+            "( \")  Bye! I said with a disappointed look.\n" +
+            "|| \\\n" +
+            "|_||\n" +
+            "____________________________________________________________";
+
+    private static final String LOGO = " _    _      _                          \n" +
+            "| |  | |    | |                         \n" +
+            "| |  | | ___| | ___ ___  _ __ ___   ___ \n" +
+            "| |/\\| |/ _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\\n" +
+            "\\  /\\  /  __/ | (_| (_) | | | | | |  __/\n" +
+            " \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___|";
+
+    private static final int INDEX_AFTER_DONE = 5;
+    private static final int INDEX_AFTER_TODO = 5;
+    private static final int INDEX_AFTER_DEADLINE = 9;
+    private static final int INDEX_AFTER_EVENT = 6;
+
     public static void main(String[] args) {
-        final String GREET = "____________________________________________________________\n" +
-                " __\n" +
-                "('')  Hello! I'm MK.II Titan with feelings.\n" +
-                "// \\  What can I do for you?\n" +
-                "|_/| \n" +
-                "____________________________________________________________\n";
-
-        final String END = "____________________________________________________________\n" +
-                " __\n" +
-                "( \")  Bye! I said with a disappointed look.\n" +
-                "|| \\\n" +
-                "|_||\n" +
-                "____________________________________________________________";
-
-        final String LOGO = " _    _      _                          \n" +
-                "| |  | |    | |                         \n" +
-                "| |  | | ___| | ___ ___  _ __ ___   ___ \n" +
-                "| |/\\| |/ _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\\n" +
-                "\\  /\\  /  __/ | (_| (_) | | | | | |  __/\n" +
-                " \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___|";
-
         String userInput;
-        Scanner in = new Scanner(System.in);
         boolean isActive = true;
+        Scanner in = new Scanner(System.in);
         TaskManager taskManager = new TaskManager();
 
+        printGreeting();
+        while (isActive) {
+            userInput = readUserInput(in);
+            isActive = checkIfUserQuits(userInput);
+            handleUserInput(taskManager, userInput);
+        }
+        printGoodbye();
+    }
+
+    private static void printGreeting() {
         System.out.println(LOGO);
         System.out.println(GREET);
-        while (isActive) {
-            userInput = in.nextLine();
+    }
 
-            if (userInput.equals("bye")) {
-                isActive = false;
-            } else if (userInput.equals("list")) {
-                taskManager.printAllTasks();
-            } else if (userInput.contains("done")) {
-                taskManager.setTaskDone(userInput.substring(5));
-            } else if (userInput.contains("todo")) {
-                taskManager.addTask(TaskType.TODO, userInput.substring(5));
-            } else if (userInput.contains("deadline")) {
-                taskManager.addTask(TaskType.DEADLINE, userInput.substring(9));
-            } else if (userInput.contains("event")) {
-                taskManager.addTask(TaskType.EVENT, userInput.substring(6));
-            }
-        }
+    private static void printGoodbye() {
         System.out.println(END);
+    }
+
+    private static String readUserInput(Scanner in) {
+        return in.nextLine();
+    }
+
+    private static boolean checkIfUserQuits(String userInput) {
+        return !userInput.equals("bye");
+    }
+
+    private static void handleUserInput(TaskManager taskManager, String userInput) {
+        if (userInput.equals("list")) {
+            taskManager.printAllTasks();
+        } else if (userInput.contains("done")) {
+            taskManager.setTaskDone(userInput.substring(INDEX_AFTER_DONE));
+        } else if (userInput.contains("todo")) {
+            taskManager.addTask(TaskType.TODO, userInput.substring(INDEX_AFTER_TODO));
+        } else if (userInput.contains("deadline")) {
+            taskManager.addTask(TaskType.DEADLINE, userInput.substring(INDEX_AFTER_DEADLINE));
+        } else if (userInput.contains("event")) {
+            taskManager.addTask(TaskType.EVENT, userInput.substring(INDEX_AFTER_EVENT));
+        }
     }
 }
