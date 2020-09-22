8dfc93722584f8cd596036454592d4c4fb70c293
AndreWongZH <wongandre98@gmail.com>
2020-09-22 17:06:01 +0800

    Add JavaDoc comments for Ui classes
    No comments added if the methods are pretty obvious at what they
    are doing.

--- src/main/java/duke/ui/InputUi.java
+++ src/main/java/duke/ui/InputUi.java
@@ -2,6 +2,12 @@ package duke.ui;
 
 import java.util.Scanner;
 
+/**
+ * Represents the printing of greetings to user.
+ * Represents the receiving of inputs from user.
+ *
+ * Methods here are pretty self-explanatory.
+ */
 public class InputUi extends Ui {
     private static final String SEPARATOR = "____________________________________________________________";
     private static final String GREET =
@@ -9,15 +15,12 @@ public class InputUi extends Ui {
             "('')  Hello! I'm MK.II Titan with feelings.\n" +
             "// \\  What can I do for you?\n" +
             "|_/|";
-
-
     private static final String LOGO = " _    _      _\n" +
             "| |  | |    | |\n" +
             "| |  | | ___| | ___ ___  _ __ ___   ___\n" +
             "| |/\\| |/ _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\\n" +
             "\\  /\\  /  __/ | (_| (_) | | | | | |  __/\n" +
             " \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___|";
-
     private static final String MESSAGE_GOODBYE =
             " __\n" +
             "( \")  Bye! I said with a disappointed look.\n" +
@@ -30,6 +33,13 @@ public class InputUi extends Ui {
         in = new Scanner(System.in);
     }
 
+    /**
+     * Returns the string the user has typed.
+     * User input is trimmed of any whitespaces at the ends of the strings.
+     * If user input is empty, then it returns null.
+     *
+     * @return User's input;
+     */
     public String readUserInput() {
         String userInput =  in.nextLine().trim();
         if (hasEmptyString(userInput)) {
--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -1,11 +1,21 @@
 package duke.ui;
 
+import static java.util.stream.Collectors.toList;
+
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Optional;
 
+/**
+ * Represents the base of printing of user's ui on the output.
+ *
+ * @see CommandUi For output strings regarding commands.
+ * @see FileUi For output strings regarding file handling.
+ * @see InputUi For taking in user's input and print greetings.
+ * @see TaskUi For output strings regarding tasks actions.
+ */
 public abstract class Ui {
     private static final String BORDER_CORNER = "+";
     private static final String BORDER_HORIZONTAL = "-";
@@ -19,7 +29,12 @@ public abstract class Ui {
         this.out = System.out;
     }
 
-    public void generateTextBorder(String outputText) {
+    /**
+     * Prints out the output text encased with borders.
+     *
+     * @param outputText Output text to be printed out
+     */
+    protected void generateTextBorder(String outputText) {
         int stringLen = outputText.length();
         printTopBottomBorder(stringLen);
         printSideBorder(stringLen);
@@ -28,8 +43,15 @@ public abstract class Ui {
         printTopBottomBorder(stringLen);
     }
 
-    public void generateMultiLineTextBorder(String... outputTexts) {
-        int stringLen = getMaxOutputLength(outputTexts);
+    /**
+     * Prints out the lists of output texts encased with borders.
+     *
+     * @param outputTexts A list of output text to be printed out
+     */
+    protected void generateMultiLineTextBorder(String... outputTexts) {
+        ArrayList<String> convertedOutputText = (ArrayList<String>) Arrays.stream(outputTexts).collect(toList());
+        int stringLen = getMaxOutputLength(convertedOutputText);
+
         printTopBottomBorder(stringLen);
         printSideBorder(stringLen);
         Arrays.stream(outputTexts).forEach((text) -> printOuterTextBorder(text, stringLen));
@@ -37,8 +59,15 @@ public abstract class Ui {
         printTopBottomBorder(stringLen);
     }
 
-    public void generateMultiLineTextBorder(ArrayList<String> outputTexts) {
+    /**
+     * Prints out the list of tasks encased with borders.
+     * This is used when list/find command is called.
+     *
+     * @param outputTexts An ArrayList of tasks to be printed out
+     */
+    protected void generateMultiLineTextBorder(ArrayList<String> outputTexts) {
         int stringLen = getMaxOutputLength(outputTexts);
+
         printTopBottomBorder(stringLen);
         printSideBorder(stringLen);
         outputTexts.forEach((text) -> printOuterTextBorder(text, stringLen));
@@ -46,15 +75,18 @@ public abstract class Ui {
         printTopBottomBorder(stringLen);
     }
 
-    public void printTopBottomBorder(int stringLen) {
+    /* Creates the top and bottom borders */
+    private void printTopBottomBorder(int stringLen) {
         out.println(BORDER_CORNER + BORDER_HORIZONTAL.repeat(stringLen + PAD_LENGTH) + BORDER_CORNER);
     }
 
-    public void printSideBorder(int stringLen) {
+    /* Creates the side borders */
+    private void printSideBorder(int stringLen) {
         out.println(BORDER_VERTICAL + BORDER_SPACING.repeat(stringLen + PAD_LENGTH) + BORDER_VERTICAL);
     }
 
-    public void printOuterTextBorder(String outputText, int maxLength) {
+    /* Wraps the output text with border string */
+    private void printOuterTextBorder(String outputText, int maxLength) {
         int extraPadding = maxLength - outputText.length();
         out.println(BORDER_VERTICAL
                 + BORDER_SPACING.repeat(PAD_LENGTH / 2)
@@ -62,16 +94,12 @@ public abstract class Ui {
                 + extraPadding) + BORDER_VERTICAL);
     }
 
-    private int getMaxOutputLength(String[] outputTexts) {
-        int maxLengthText = 0;
-        Optional<String> streamData = Arrays.stream(outputTexts)
-                .max(Comparator.comparingInt(String::length));
-        if (streamData.isPresent()) {
-            maxLengthText = streamData.get().length();
-        }
-        return maxLengthText;
-    }
-
+    /**
+     * Returns the length of the longest string in an array list of strings.
+     *
+     * @param outputTexts An array list of strings.
+     * @return length of the longest string.
+     */
     private int getMaxOutputLength(ArrayList<String> outputTexts) {
         int maxLengthText = 0;
         Optional<String> streamData = outputTexts.stream()
