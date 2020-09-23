80464a9105a1e49be446881e127b922a25d853b4
AndreWongZH <wongandre98@gmail.com>
2020-09-23 20:26:26 +0800

    Make find command case insensative
    Add in additional functionaility for find to search keywords to be
    case insensative.

--- src/main/java/duke/task/TaskManager.java
+++ src/main/java/duke/task/TaskManager.java
@@ -225,7 +225,7 @@ public class TaskManager implements TaskAction {
      * @return An ArrayList of tasks filtered by filterString.
      */
     private ArrayList<Task> filterByString(String filterString) {
-        return (ArrayList<Task>) tasks.stream().filter((t) -> t.getDescription().contains(filterString))
+        return (ArrayList<Task>) tasks.stream().filter((t) -> t.getDescription().toLowerCase().contains(filterString.toLowerCase()))
                 .collect(toList());
     }
 
